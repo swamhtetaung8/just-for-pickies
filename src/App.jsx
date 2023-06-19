b01e0d234938f96e00502c6325e77705b7e610c1
bcmm.intern06 <bcmm.intern06@brycenmyanmar.com.mm>
2023-06-19 11:21:36 +0630

    fix loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieContextProvider from "./context/MovieContext";
 import Home from "./pages/Home";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Loader } from "@mantine/core";
 
 const queryClient = new QueryClient();
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <MovieContextProvider>
-        <Home />
+        {
+          loading ? (
+            <div className="flex items-center justify-center w-full h-screen">
+              <Loader size="xl" variant="dots" />
+            </div>
+          ):(
+            <Home />
+          )
+        }
       </MovieContextProvider>
     </QueryClientProvider>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,26 +4,13 @@ import MovieSelect from "../components/MovieSelect";
 import { Loader } from "@mantine/core";
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+  
   return (
     <main className="bg-gray-50">
-      {loading ? (
-        <div className="flex items-center justify-center w-full h-screen">
-          <Loader size="xl" variant="dots" />
-        </div>
-      ) : (
-        <>
+      
           <Hero />
           <MovieSelect />
-        </>
-      )}
+    
     </main>
   );
 };
